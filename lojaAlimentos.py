9a484d348a878cd1de54941e9805aa311566fc39
Leonardodouglastech <167647313+Leonardodouglastech@users.noreply.github.com>
2024-06-01 20:14:16 +0000

    Inicializando codigo

--- lojaAlimentos.py
+++ lojaAlimentos.py
@@ -1,20 +1,30 @@
 class Produto:
-    def __init__(self, nome, codigo, valor, qtd):
-        self.nome = nome
-        self.codigo = codigo
-        self.valor = valor
-        self.qtd = qtd
+    def __init__(self, nome:str, cod:int, valor:float, qtd:int):
+        self.__nome = nome
+        self.__cod = cod
+        self.__valor = valor
+        self.__qtd = qtd
+    
+    def exibirInfor(self):
+         return(f'Nome: {self.__nome}| Código: {self.__cod}| Valor: R${self.__valor}| Quantidade: {self.__qtd}')
+
+    
+    def adicionar(self, qtd:int=1):
+        self.__qtd += qtd
+
+    def remover(self, qtd:int=1):
+        self.__qtd -= qtd
 
 
 class Alimento(Produto):
-    def __init__(self, nome, codigo, valor, qtd, validade, peso):
-        super().__init__(nome, codigo, valor, qtd)
+    def __init__(self, nome, cod, valor, qtd, validade, peso):
+        super().__init__(nome, cod, valor, qtd)
         self.validade = validade
         self.peso = peso
 
 class Bebidas(Produto):
-    def __init__(self, nome, codigo, valor, qtd, alcoolica, ml):
-        super().__init__(nome, codigo, valor, qtd)
+    def __init__(self, nome, cod, valor, qtd, alcoolica, ml):
+        super().__init__(nome, cod, valor, qtd)
         self.acoolica = alcoolica
         self.ml = ml
         
